37295b5c53fdb23c1c05502faf5b2166f20786ac
Stas Pyatunin <staspyatunin@gmail.com>
2025-04-11 23:00:52 +0300

    making some models incremental (core_dim_inventory and stg_films__inventory)

--- models/core/films/core_dim_inventory.sql
+++ models/core/films/core_dim_inventory.sql
@@ -1,6 +1,17 @@
 {{
   config(
-    materialized = 'table'
+    materialized = 'incremental',
+    incremental_strategy = 'merge',
+    unique_key = ['inventory_id'],
+    on_schema_change = 'fail',
+    merge_exclude_columns = ['inventory_pk'],
+    pre_hook = '
+        delete from core.core_dim_inventory
+        where inventory_id in (
+            select inventory_id from staging.stg_films__inventory
+            where deleted is not null
+        )
+    '
     )
 }}
 
@@ -12,7 +23,12 @@ select
     f.rental_duration as rental_duration,
     f.rental_rate as rental_rate,
     f.length as length,
-    f.rating as rating
+    f.rating as rating,
+    i.last_update as last_update
 from
     {{ ref('stg_films__inventory') }} i join
     {{ ref('stg_films__film')}} f on i.film_id = f.film_id
+{% if is_incremental() %}
+where i.last_update > (select max(last_update) from {{ this }}) and
+      i.deleted is null
+{% endif %}
--- models/staging/films/stg_films__inventory.sql
+++ models/staging/films/stg_films__inventory.sql
@@ -1,12 +1,29 @@
 {{
   config(
-    materialized = 'table'
+    materialized = 'incremental',
+    incremental_strategy = 'merge',
+    unique_key = ['inventory_id'],
+    on_schema_change = 'fail'
     )
 }}
+
+{% if is_incremental() %}
+with max_update as (
+    select max(last_update) as max_last_update from {{ this }}
+)
+{% endif %}
+
 
 select
     inventory_id,
     film_id,
-    store_id
+    store_id,
+    last_update,
+    deleted
 from
     {{ source('film_src', 'inventory') }}
+
+{% if is_incremental() %}
+where last_update > (select max_last_update from max_update) or
+      (deleted is not null and (inventory_id in (select inventory_id from {{ this }} where deleted is null)))
+{% endif %}
