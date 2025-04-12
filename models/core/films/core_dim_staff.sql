40cd56d21594ee27b9194563d2cad046c45d9309
Stas Pyatunin <staspyatunin@gmail.com>
2025-04-12 18:33:20 +0300

    making models called core_dim_staff and stg_films__staff incremental

--- models/core/films/core_dim_staff.sql
+++ models/core/films/core_dim_staff.sql
@@ -1,6 +1,17 @@
 {{
   config(
-    materialized = 'table'
+    materialized = 'incremental',
+    incremental_strategy = 'merge',
+    unique_key = ['staff_id'],
+    on_schema_change = 'fail',
+    merge_exclude_columns = ['staff_pk'],
+    pre_hook = '
+              delete from core.core_dim_staff
+              where staff_id in
+                    (select staff_id 
+                     from staging.stg_films__staff
+                     where active = false)
+    '
     )
 }}
 
@@ -11,9 +22,14 @@ select
     stf.last_name as last_name,
     a.address as address,
     a.district as district,
-    c.city as city
+    c.city as city,
+    stf.last_update as last_update
 from
     {{ ref('stg_films__staff') }} stf join
     {{ ref('stg_films__store') }} str on stf.store_id = str.store_id join
     {{ ref('stg_films__address') }} a on str.address_id = a.address_id join
     {{ ref('stg_films__city') }} c on c.city_id = a.city_id
+{% if is_incremental() %}
+where stf.last_update > (select max(last_update) from {{ this }}) and
+      stf.active = true
+{% endif %}
--- models/staging/films/stg_films__staff.sql
+++ models/staging/films/stg_films__staff.sql
@@ -1,6 +1,9 @@
 {{
   config(
-    materialized = 'table'
+    materialized = 'incremental',
+    incremental_strategy = 'merge',
+    unique_key = ['staff_id'],
+    on_schema_change = 'fail'
     )
 }}
 
@@ -8,6 +11,13 @@ select
     staff_id,
     first_name,
     last_name,
-    store_id
+    store_id,
+    last_update,
+    active
 from 
     {{ source('film_src', 'staff') }}
+
+{% if is_incremental() %}
+where last_update > (select last_update from {{ this }} order by last_update desc limit 1)
+{% endif %}
+
